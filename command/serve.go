bde777ec65591f9fb393027cce10786f41ec36eb
tcz001 <fan.torchz@gmail.com>
2019-11-04 14:35:45 +0800

    add CORS

--- command/serve.go
+++ command/serve.go
@@ -357,7 +357,18 @@ type handler struct {
 	*ServeCommand
 }
 
+func setupCORS(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	setupCORS(&w)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
 	cmd_args, _ := r.URL.Query()["args"]
 	log.Printf("[INFO] CLI command args: %#v", cmd_args)
 
